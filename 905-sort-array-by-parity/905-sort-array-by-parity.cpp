4a9cc9021e0a60a30aced03b07609dd3e47a358f
dang-young <100173589+dang-young@users.noreply.github.com>
2022-02-26 08:33:04 +0900

    Time: 11 ms (74.17%), Space: 16.2 MB (82.68%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -4,20 +4,19 @@ public:
         int n = nums.size();
         int pos1=0, pos2=n-1;
         int tmp;
-        while(nums[pos2]%2==1 && pos2>0) pos2--;
-        cout<<"pos2:"<<pos2<<endl;
+        while(pos2>0 && nums[pos2]%2==1) pos2--;
+        while(pos1<n && nums[pos1]%2==0) pos1++;
+
         while(pos1<pos2){
             if(nums[pos1]%2==0) pos1++;
             else{
-                bool find = false;
-                while(!find && pos2>pos1){
+                while(pos2>pos1){
                     if(nums[pos2]%2==0){
-                        cout<<"SWAP!!! pos1: "<<nums[pos1]<<", pos2:"<<nums[pos2]<<endl;
-                        find = true;
                         tmp = nums[pos2];
                         nums[pos2] = nums[pos1];
                         nums[pos1] = tmp;
                         pos2--;
+                        break;
                     }
                     else pos2--;
                 }
