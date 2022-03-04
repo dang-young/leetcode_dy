7512a886674644badb353cdb26694e57efcf8d71
dang-young <100173589+dang-young@users.noreply.github.com>
2022-03-04 17:10:11 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (89.42%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -6,17 +6,9 @@ public:
         bool flag = false;
         for(int i=0; i<len; i++){
             if(s[i]==s[i+1]){
-                if(i==len-2){
-                    if(flag) return "";
-                    else{
-                        reverse(s.begin(), s.end());
-                        flag = true;
-                        i=-1; continue;
-                    }
-                }
                 j=i+1;
                 while(s[i+1]==s[j]) j++;
-                if(j>=len) {
+                if(i==len-2 || j>=len){
                     if(flag) return "";
                     else{
                         reverse(s.begin(), s.end());
