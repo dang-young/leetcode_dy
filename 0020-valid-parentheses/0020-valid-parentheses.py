aec2c2b016cd713ec836615766ee70a091d3c4cc
dang-young <100173589+dang-young@users.noreply.github.com>
2023-08-08 20:16:44 +0900

    Time: 28 ms (99.43%), Space: 16.4 MB (13.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,5 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        """
-        stack = deque()
-        for c in s:
-            if c=='(' or c=='{' or c=='[':
-                stack.append(c)
-            elif c==')' or c=='}' or c==']':
-                if len(stack)==0: return False
-                last = stack.pop()
-                if last=='(' and c!=')': return False
-                if last=='{' and c!='}': return False
-                if last=='[' and c!=']': return False
-        if len(stack)!=0: return False
-        return True
-        """
         stack = []
         match = {'(':')', '{':'}', '[':']'} #dictionary
         
